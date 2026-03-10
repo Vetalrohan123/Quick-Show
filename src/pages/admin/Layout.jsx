17e1c59eaadab6ab9329fab4341515da8bbdb52a
Rohan Vetal <vetalrohan61@gmail.com>
2026-03-10 06:52:22 -0700

    fixed admin sidebar import

--- src/pages/admin/Layout.jsx
+++ src/pages/admin/Layout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import AdminNavbar from '../../components/admin/AdminNavbar'
-import AdminSidebar from '../../components/admin/adminSidebar'
+import AdminSidebar from '../../components/admin/AdminSidebar'
 import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
